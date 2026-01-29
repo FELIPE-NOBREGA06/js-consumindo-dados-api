2d2610e6faa194739ae1f5589d3f31a1b86da102
CLEVERSON  SANTOS <felipecleverson1@gmail.com>
2026-01-29 12:13:32 -0400

    feat: criando api

---
+++ script.js
@@ -0,0 +1,18 @@
+/** @format */
+
+async function buscaEndereco(cep) {
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
